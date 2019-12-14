a81ad643ea355095dc9b620d4f4e0b571871fa1c
MinexAutomation <minex.automation@gmail.com>
2019-12-14 01:55:43 -0800

    Added IServiceCollection extension to configure a GitHubAuthentication.

--- source/R5T.Polidea.Sardinia/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Polidea.Sardinia
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Polidea.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Sardinia;
+
+
+namespace R5T.Polidea.Sardinia
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGitHubAuthentication(this IServiceCollection services)
+        {
+            services
+                .AddOptions()
+                .Configure<GitHubAuthentication>()
+                ;
+
+            return services;
+        }
+    }
+}
